550f10877970ec9c6e8078a3ccb18c2130f8f006
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-12-20 22:08:46 +0300

    add sender

--- RabbitMQ.Send/Program.cs
+++ RabbitMQ.Send/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "god's in his heaven, all's right with the world",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+const string message = "Hello there!";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(
+    exchange: string.Empty,
+    routingKey: "hello",
+    basicProperties: null,
+    body: body
+);
+
+Console.WriteLine($"x sent {message}");
+Console.WriteLine("Press enter to exit");
+
+Console.ReadLine();
